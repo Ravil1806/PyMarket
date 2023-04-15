9504cc6dac29df3cc2f6fbde5c7aaa64da672f2a
ravil1806 <ravil1806@mail.ru>
2023-04-15 19:39:54 +0600

    +icon +страница(Вход) +страница(Регистрация)

--- main.py
+++ main.py
@@ -5,8 +5,18 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('base.html')
+    return render_template('base.html', title='Главная страница')
+
+
+@app.route('/sign_in', methods=['GET', 'POST'])
+def signin():
+    return render_template('sign_in.html', title='Авторизация')
+
+
+@app.route('/sign_up', methods=['GET', 'POST'])
+def signup():
+    return render_template('sign_up.html', title='Регистрация')
 
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=5555)
+    app.run(host='127.0.0.1', port=5000, debug=True)
